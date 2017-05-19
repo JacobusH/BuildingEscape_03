9e2fedf562adbdaff6fe7155943c1e56e3c9fdb1
Jacobus <jahardin@uci.edu>
2017-05-18 17:04:29 -0700

    first commit

---
+++ Source/BuildingEscape_03/BuildingEscape_03GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEscape_03GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_03_API ABuildingEscape_03GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
